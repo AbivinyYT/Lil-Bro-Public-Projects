9760daba551a0db709e435473aa3c91082b36fcc
abiviny.mp4 <235961826+AbivinyYT@users.noreply.github.com>
2025-12-12 23:11:34 -0300

    first project in python

---
+++ cubo.py
@@ -0,0 +1,8 @@
+name = input("Say your name: ")
+age = int(input("Say your age: "))
+
+actual_year = 2025
+birth_year = 2025 - age
+
+print("\nHello, ", name + "!")
+print("\nYou're probably born in ", birth_year)
